8d3a71488b356d624e2ca99cf157936262aae46d
Yuya <yuyayoshida87@gmail.com>
2015-05-04 18:36:39 -0400

    initial commit, not working

--- floor.rb
+++ floor.rb
@@ -3,12 +3,14 @@ require '/.passenger.rb'
 require '/.elevator.rb'
 
 class Floor
-	attr_reader :floor_num, :waiting_list
+	attr_reader :floor_num, :waiting_list, :curr_floor
 	def initialize(floor_num, waiting_list)
 		@floor_num = floor_num
 		@waiting_list = Array.new
 	end
 
 	def get_floor
+		return @curr_floor
+	end
 
 end
